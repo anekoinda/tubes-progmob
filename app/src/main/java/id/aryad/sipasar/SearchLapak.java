d41af6c18c9831e726da8f17fec9f2cfbcf0444f
Shilta Inda <shiltainda@gmail.com>
2021-05-06 03:47:51 +0800

    create login

---
+++ app/src/main/java/id/aryad/sipasar/SearchLapak.java
@@ -0,0 +1,14 @@
+package id.aryad.sipasar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchLapak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_lapak);
+    }
+}
