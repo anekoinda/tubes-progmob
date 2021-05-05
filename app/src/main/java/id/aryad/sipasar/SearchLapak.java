2480314ded3cd51c5e9d6e9dac52b930b91a6da0
Shilta Inda <shiltainda@gmail.com>
2021-05-06 04:04:27 +0800

    create search lapak

--- app/src/main/java/id/aryad/sipasar/SearchLapak.java
+++ app/src/main/java/id/aryad/sipasar/SearchLapak.java
@@ -1,14 +1,17 @@
 package id.aryad.sipasar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class SearchLapak extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class SearchLapak extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_lapak);
+
     }
 }
